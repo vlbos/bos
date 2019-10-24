cb91e9ea519bf196d8245e82db8423a1aadd1313
vlbos <vlbos2018@gmail.com>
2019-10-24 10:45:42 +0800

    remove eos-vm folder

--- libraries/eos-vm/external/outcome/include/outcome/quickcpplib/doc/html/search/files_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['execinfo_5fwin64_2eh',['execinfo_win64.h',['../execinfo__win64_8h.html',1,'']]]
-];
--- libraries/eos-vm/external/outcome/include/outcome/quickcpplib/doc/html/search/files_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['import_2eh',['import.h',['../import_8h.html',1,'']]]
-];
